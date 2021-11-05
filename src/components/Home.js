3c5a2dcbb17829d7c58dc53ea25ac47f9af97eb9
Carrington Jones <carrington.jones47@gmail.com>
2021-11-05 14:32:25 -0500

    Completed SearchBar logic. Working in application

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ import {useHomeFetch} from '../hooks/useHomeFetch'
 import NoImage from '../images/no_image.jpeg';
 
 const Home = () => {
-    const {state, loading, error} = useHomeFetch();
+    const {state, loading, error, setSearchTerm} = useHomeFetch();
 
     console.log(state)
 
@@ -28,7 +28,7 @@ const Home = () => {
                     text={state.results[0].overview}
                 />
             ) : null}
-            <SearchBar/>
+            <SearchBar setSearchTerm={setSearchTerm} />
             <Grid header='Popular Movies'>
                 {state.results.map(movie => (
                     <Thumb
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -12,6 +12,8 @@ const initialState = {
 //ALWAYS use "use" to start customer hooks
 
 export const useHomeFetch = () => {
+    const [searchTerm, setSearchTerm] = useState('')
+
     const [state, setState] = useState(initialState)
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
@@ -40,5 +42,5 @@ export const useHomeFetch = () => {
         fetchMovies(1)
     }, []);
 
-    return {state, loading, error};
+    return {state, loading, error, setSearchTerm};
 }
