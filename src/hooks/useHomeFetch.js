1e7bd23088c4cab20632b0fea8278dff5111380f
Carrington Jones <carrington.jones47@gmail.com>
2021-11-07 20:02:39 -0600

    Completed search bar component

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -37,10 +37,11 @@ export const useHomeFetch = () => {
         setLoading(false);
     }
 
-    //Initial movie render
+    //Initial and search
     useEffect(() => {
-        fetchMovies(1)
-    }, []);
+        setState(initialState)
+        fetchMovies(1, searchTerm)
+    }, [searchTerm]);
 
-    return {state, loading, error, setSearchTerm};
+    return {state, loading, error, searchTerm, setSearchTerm};
 }
