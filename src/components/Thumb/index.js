fa9a5aef423cb83802eb965bf9f893744f997908
Carrington Jones <carrington.jones47@gmail.com>
2021-11-09 22:35:02 -0600

    Completed routing for application

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,12 +1,21 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 //Styles
-import { Image } from './Thumb.styles';
+import {Image} from './Thumb.styles';
 
-const Thumb = ({ image, movieId, clickable}) => (
+const Thumb = ({image, movieId, clickable}) => (
     <div>
-        <Image src={image} alt='movie-thumb' />
+        {clickable ? (
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt='movie-thumb'/>
+            </Link>
+        ) : (
+            <Image src={image} alt='movie-thumb'/>
+        )}
     </div>
 );
 
 export default Thumb;
+
+//Line 9 is ternary operator checking to see if movie thumbnail is clickable
