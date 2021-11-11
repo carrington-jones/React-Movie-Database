348ec6c024c4d93db12c9544a994f2c1a0104ddf
Carrington Jones <carrington.jones47@gmail.com>
2021-11-11 11:42:02 -0600

    Created movie BreadCrumb component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,6 +4,7 @@ import {useParams} from 'react-router-dom'
 import {IMAGE_BASE_URL, POSTER_SIZE} from "../config";
 
 //Components
+import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 //Hook
@@ -14,10 +15,14 @@ import NoImage from '../images/no_image.jpeg';
 const Movie = () => {
     const {movieId} = useParams();
 
-    const {state: movie, loading, error} = useMovieFetch(movieId)
+    const {state: movie, loading, error} = useMovieFetch(movieId);
+
+    if (loading) return <Spinner/>
+    if (error) return <div>Something went wrong!</div>
+
     return (
         <React.Fragment>
-            <div>Movie</div>
+            <BreadCrumb movieTitle={movie.original_title}/>
         </React.Fragment>
     )
 }
