d08b16615236f71aebb5e881dcb2b4a0c04b3530
Carrington Jones <carrington.jones47@gmail.com>
2021-11-12 16:28:15 -0600

    Created and completed Actors grid component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,6 +9,7 @@ import Grid from "./Grid";
 import Spinner from "./Spinner";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 //Hook
 import {useMovieFetch} from "../hooks/useMovieFetch";
 //Image
@@ -27,8 +28,23 @@ const Movie = () => {
             <BreadCrumb movieTitle={movie.original_title}/>
             <MovieInfo movie={movie}/>
             <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue}/>
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path
+                                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                                : NoImage
+                        }
+                    />
+                ))}
+            </Grid>
         </React.Fragment>
     )
 }
+
 
 export default Movie;
